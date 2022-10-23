dfd251a7c44d13acb629a305ed62f080ca46ba30
HemarajKS <hemaraja.shettigar@robosoftin.com>
2022-10-23 08:09:02 +0530

    first commit

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -10,8 +10,17 @@ const Body = (props: any) => {
   console.log('fetched data', Fetch());
 
   const place: any = GeoCode(props.props);
+  console.log('====================================');
+  console.log(place);
+  console.log('====================================');
 
-  return <div className="body">Data</div>;
+  return (
+    <div className="body">
+      {place
+        ? JSON.stringify(place.results[0].location)
+        : JSON.stringify({ lat: 13.340905, lng: 74.742119 })}
+    </div>
+  );
 };
 
 export default Body;
